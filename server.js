e2b2edeb8101ca3db472481058d33a67d72e8364
Alvin Chong <alvinchong92@gmail.com>
2016-11-22 01:15:42 -0500

    make things better

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080);
